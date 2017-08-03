440b166ac3f1b7f7493394cf5dd2bf7b6b4fb371
TRQ1 <trq1.lee@gmail.com>
2017-08-03 11:28:07 -0400

    change variables

--- docker-swarm/terraform/docker-swarm.tf
+++ docker-swarm/terraform/docker-swarm.tf
@@ -25,7 +25,7 @@ resource "google_compute_instance" "swarm-manager-main" {
       "if ! ${var.swarm_init}; then docker swarm init; fi",
       "echo ${var.doker_secret_jekins_id} | docker secret create jenkins-user -", 
       "echo ${var.doker_secret_jekins_pass} | docker secret create jenkins-pass -", 
-      "echo `-master http://master:8080 -password ${var.doker_secret_jekins_pass} -username ${var.doker_secret_jekins_id}`|docker secret create jenkins -",
+      "echo '-master http://master:8080 -password ${var.doker_secret_jekins_pass} -username ${var.doker_secret_jekins_id}' |docker secret create jenkins -",
       "export FRIST_JENKINS_IMAGE=$(docker images | grep ${var.image_name} |awk '{print $3}')",
       "docker tag $FRIST_JENKINS_IMAGE ${var.gcp_hostname}/${var.gcp_project_id}/${var.image_name}",
       "gcloud docker -- push ${var.gcp_hostname}/${var.gcp_project_id}/${var.image_name}",
@@ -42,6 +42,7 @@ resource "google_compute_instance" "swarm-manager-main" {
 
 
 resource "google_compute_instance" "swarm-manager" {
+  count        = "${var.swarm_managers_count}"
   name         = "${var.swarm_manager_jenkins}-${count.index}"
   machine_type = "${var.swarm_instance_type}"
   zone         = "${var.gcp_zone}"
@@ -64,8 +65,8 @@ resource "google_compute_instance" "swarm-manager" {
   provisioner "remote-exec" {
     inline = [
       "gcloud docker -- pull ${var.gcp_hostname}/${var.gcp_project_id}/${var.image_name}",
-      "export WORKER=$(sudo ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key ${var.user_id}@                      ${google_compute_instance.swarm-manager-main.network_interface.0.address} docker swarm join-token manager)",
-      "docker swarm join ${google_compute_instance.swarm-manager.network_interface.0.address}:2377 --token $WORKER",
+      "export MANAGER_TOKEN=$(sudo ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key ${var.user_id}@${google_compute_instance.swarm-manager-main.network_interface.0.address} docker swarm join-token manager -q)",
+      "docker swarm join ${google_compute_instance.swarm-manager-main.network_interface.0.address}:2377 --token $MANAGER_TOKEN",
       "rm -f ~/key" 
     ]
   }
@@ -79,7 +80,7 @@ resource "google_compute_instance" "swarm-manager" {
 
 
 resource "google_compute_instance" "swarm-worker" {
-  count        = "${var.swarm_workers}"
+  count        = "${var.swarm_workers_count}"
   name         = "${var.swarm_workers_name}-${count.index}"
   machine_type = "${var.swarm_instance_type}"
   zone         = "${var.gcp_zone}"
@@ -102,8 +103,8 @@ resource "google_compute_instance" "swarm-worker" {
   provisioner "remote-exec" {
     inline = [
       "gcloud docker -- pull ${var.gcp_hostname}/${var.gcp_project_id}/${var.image_name}",
-      "export WORKER=$(sudo ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key ${var.user_id}@${google_compute_instance.swarm-manager-main.network_interface.0.address} docker swarm join-token worker -q)",
-      "docker swarm join ${google_compute_instance.swarm-manager-main.network_interface.0.address}:2377 --token $WORKER",
+      "export WORKER_TOKEN=$(sudo ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key ${var.user_id}@${google_compute_instance.swarm-manager-main.network_interface.0.address} docker swarm join-token worker -q)",
+      "docker swarm join ${google_compute_instance.swarm-manager-main.network_interface.0.address}:2377 --token $WORKER_TOKEN",
       "rm -f ~/key"
     ]
  }
---
+++ docker-swarm/terraform/varialbe-sample.tf
@@ -0,0 +1,70 @@
+variable "swarm_manager_token" {
+  default = ""
+}
+variable "swarm_worker_token" {
+  default = ""
+}
+variable "swarm_manager_ip" {
+  default = ""
+}
+variable "swarm_managers_count" {
+  default = ""
+}
+variable "swarm_workers_count" {
+  default = ""
+}
+variable "swarm_instance_type" {
+  default = ""
+}
+variable "swarm_init" {
+  default = 
+}
+variable "swarm_manager_jenkins_main" {
+  default = ""
+}
+variable "swarm_manager_jenkins" {
+  default = ""
+}
+variable "swarm_workers_name" {
+  default = ""
+}
+variable "gcp_region" {
+  default = ""
+}
+variable "gcp_zone" {
+  default = ""
+}
+variable "gcp_project_id" {
+  default = ""
+}
+variable "key_json" {
+  default = ""
+}
+variable "worker_source_disk_image" {
+  default = ""
+}
+variable "master_source_disk_image" {
+  default = ""
+}
+variable "doker_secret_jekins_id" {
+  default = ""
+}
+variable "doker_secret_jekins_pass" {
+  default = ""
+}
+variable "gcp_hostname" {
+  default = ""
+}
+variable "image_name" {
+  default = ""
+}
+variable "user_id" {
+  default = ""
+}
+variable "connect_key" {
+  default = ""
+}
+variable "ssh_key_file" {
+  default = ""
+}
+
